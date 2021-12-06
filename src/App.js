68ed129f5fdb65629170c06bfcfea92f9a90ee7a
Yqodsi <49367257+yqodsi@users.noreply.github.com>
2021-12-06 07:07:13 +0100

    learning

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView
+} from "react-native";
 
 function App() {
-  const [outputText, setOutputText] = useState("Hello!");
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [CourseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals((currentGoals) => [...CourseGoals, enteredGoal]);
+  };
   return (
-    <View style={styles.container}>
-      <Text> {outputText} </Text>
-      <Button
-        title="Change text"
-        onPress={() => setOutputText("The text changed!")}
-      />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="ADD" onPress={addGoalHandler} />
+      </View>
+      <ScrollView>
+        {CourseGoals.map((goal, index) => (
+          <View style={styles.listItem} key={index}>
+            <Text>{goal}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+  screen: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  input: {
+    width: "100%",
+    borderColor: "gray",
+    borderRadius: 10,
+    borderWidth: 1,
+    padding: 10
+  },
+  listItem: {
+    padding: 10,
+    backgroundColor: "salmon",
+    borderColor: "black",
+    marginVertical: 10,
+    borderWidth: 1
   }
 });
 
